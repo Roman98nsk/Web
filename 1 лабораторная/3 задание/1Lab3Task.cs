b571ad1d88b9d023a41e8d3b23e4cc08bf492438
Roman98nsk <yaroshev.roma@mail.ru>
2022-09-04 21:56:12 +0700

    1, 2, 3 task - complete

--- 1 лабораторная/3 задание/1Lab3Task.cs
+++ 1 лабораторная/3 задание/1Lab3Task.cs
@@ -1,14 +1,31 @@
-﻿int Fibonachi(int n)
-{
-    if (n == 0 || n == 1) return n;
-     
-    return Fibonachi(n - 1) + Fibonachi(n - 2);
+﻿Console.Write("Input number of month: ");
+int month = Convert.ToInt32(Console.ReadLine());
+
+if (month > 48) {
+    Console.WriteLine("n/a");            
+} else if (month < 0) {
+    Console.WriteLine("n/a");
+} else {
+    int  zero = 0;
+    int first = 1;
+    int sum = 0;
+    int j = 2;
+    
+    if (month == 0) {
+        Console.WriteLine($"In the end of the {month} month there were 0 pair"); 
+    } else if (month == 1 || month == 2) {
+        Console.WriteLine($"In the end of the {month} month there were 1 pair"); 
+    } else if (month > 2) {
+        while (j <= month) {
+            sum = zero + first;
+            zero = first;
+            first = sum;
+            j++;
+        }
+    Console.WriteLine($"In the end of the {month} month there were {first} pairs"); 
+    }
 }
- 
-int fib4 = Fibonachi(4);
-int fib5 = Fibonachi(5);
-int fib6 = Fibonachi(6);
- 
-Console.WriteLine($"4 число Фибоначчи = {fib4}");
-Console.WriteLine($"5 число Фибоначчи = {fib5}");
-Console.WriteLine($"6 число Фибоначчи = {fib6}");
+
+
+//0 1 2 3 4 5 6   7   8    9
+//0 1 1 2 3 5 8   13  21   34
---
+++ 1 лабораторная/4 задание/1Lab4Task.cs
@@ -0,0 +1,22 @@
+﻿/*string inputFile = @"path/File.xls";
+string outputFile = @"path/File.csv";
+
+using (Converter converter = new Converter(inputFile)) {
+    Spread
+}*/
+
+PM> Install-Package GroupDocs.Conversion
+
+string inputFile = @"path/spreadsheet.xlsx";
+string outputFile = @"path/comma-sparated-values.csv";
+
+using (Converter converter = new Converter(inputFile))
+{
+    SpreadsheetConvertOptions options = new SpreadsheetConvertOptions
+    {
+        PageNumber = 2,
+        PagesCount = 1,
+        Format = SpreadsheetFileType.Csv // Specify the conversion format
+    };
+    converter.Convert(outputFile, options);
+}
